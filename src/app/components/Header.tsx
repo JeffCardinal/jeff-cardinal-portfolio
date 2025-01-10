e0aeea1de9cb5f91e0ee58516a0f21bd3a603605
Jeff Cardinal <jeffjcardinal@gmail.com>
2025-01-09 23:52:19 -0500

    3JS implementation

--- src/app/components/Header.tsx
+++ src/app/components/Header.tsx
@@ -2,7 +2,7 @@ import { ReactNode } from 'react';
  
 export default function Header({ children }: { children: ReactNode }) {
   return (
-    <div className="lg:visible md:visible invisible overflow-hidden flex bg-sky-400 text-center items-center justify-center p-4 text-2xl absolute inset-x-0 top-0">
+    <div className="lg:visible md:visible invisible overflow-hidden flex text-center items-center justify-center p-4 text-2xl absolute inset-x-0 top-0 z-10">
       { children }
     </div>
   );
