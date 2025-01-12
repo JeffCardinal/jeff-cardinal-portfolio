b7d334493181c3d2e019168f8e4039d198188146
Jeff Cardinal <jeffjcardinal@gmail.com>
2025-01-11 21:09:26 -0500

    Restructure the video folders

--- src/app/components/VideoFeature.tsx
+++ src/app/components/VideoFeature.tsx
@@ -6,27 +6,27 @@ export default function VideoFeature() {
   return (
       <div className="box-border lg:p-8 items-center justify-center flex flex-3 flex-col lg:flex-row bg-black">
         <Video 
-        videoUrl="/videos/videos-optimized/ditb-reel.mp4"
+        videoUrl="/videos-optimized/ditb-reel.mp4"
         title={"xJermsx - Drugs in the Bathroom [DJ CAMGIRL Remix]"}
         description={""} caseStudyUrl={""} />
           {/* Remix promotional reel. Smiley pressed pill rendered in Cinema 4D and imported to Adobe AE. Custom glitch effect. */}
         <Video 
-        videoUrl="/videos/videos-optimized/funk-forever-reel.mp4"
+        videoUrl="/videos-optimized/funk-forever-reel.mp4"
         title={"Funk Forever Party"}
         description={""} caseStudyUrl={""} />
           {/* Future Funk party promotion reel. */}
         <Video 
-        videoUrl="/videos/videos-optimized/sega-dreamland-reel.mp4"
+        videoUrl="/videos-optimized/sega-dreamland-reel.mp4"
         title={"VAPERROR - Sega Dreamland Reel"}
         description={""} caseStudyUrl={""} />
           {/* 10 year anniversary commemorative reel based on the original single cover posted on SoundCloud. */}
         <Video 
-        videoUrl="/videos/videos-optimized/tribal-heart-reel.mp4"
+        videoUrl="/videos-optimized/tribal-heart-reel.mp4"
         title={"Tribal Diamond Heart"}
         description={""} caseStudyUrl={"./case-tribal-heart"} />
           {/* Rendered in Blender using Cycles. Custom material set to mimic real diamond refractive index. Graffiti background from construction zone in Montreal, shot on iPhone 13. */}
         <Video 
-        videoUrl="/videos/videos-optimized/metalheartreel1.mp4"
+        videoUrl="/videos-optimized/metalheartreel1.mp4"
         title={"DJ CAMGIRL - Aberrations"}
         description={""} caseStudyUrl={""} />
           {/* Promotional reel. Metalheart created in Blender. */}
