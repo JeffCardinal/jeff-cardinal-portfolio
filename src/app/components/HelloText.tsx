2312ef571554728459f8a9140bde9f8ada12598c
Jeff Cardinal <jeffjcardinal@gmail.com>
2025-01-13 01:08:26 -0500

    Attempt to fix front page scaling on iPhone

--- src/app/components/HelloText.tsx
+++ src/app/components/HelloText.tsx
@@ -2,14 +2,15 @@ import Image from "next/image";
 
 const HelloText: React.FC = () => {
     return (
-        <Image
-            className="absolute top-[38%] lg:left-[28%] left-[50%] -translate-x-1/2 -translate-y-full rotate-[-10deg]"
-            src={`/images/heyim.png`}
-            alt='' 
-            width={250}
-            height={250}
-            objectFit='contain'
-        />
+        <div className="absolute top-[30%] lg:left-[25%] left-[50%] -translate-x-1/2 -translate-y-1/2 flex flex-row object-scale-down">
+            <Image
+                className="rotate-[-10deg]"
+                src={`/images/heyim.png`}
+                alt='' 
+                width={250}
+                height={250}
+            />
+        </div>
     );
 };
 
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,6 +4,7 @@ import "./globals.css";
 import Header from "./components/Header";
 import Link from "next/link";
 import MobileNav from "./components/MobileNav";
+import Head from "next/head";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -19,6 +20,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <Head>
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+      </Head>
       <body className={inter.className}>
         <MobileNav>{children}</MobileNav>
         <Header>
