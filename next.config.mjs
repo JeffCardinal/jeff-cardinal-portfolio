adb91c4bfb9af68bbc549d2978e1906257beec9d
Jeff Cardinal <jeffjcardinal@gmail.com>
2025-02-02 13:15:50 -0500

    Added custom scripts for user analytics

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,17 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    rewrites: async () => {
+        return [
+            {
+                source: "/api/data/:match*",
+                destination: "https://https://jeff-cardinal-portfolio.vercel.app/_vercel/insights/:match*",
+            },
+            {
+                source: "/api/performance/:match*",
+                destination: "https://https://jeff-cardinal-portfolio.vercel.app/_vercel/speed-insights/:match*",
+            },
+        ];
+    },
+};
 
 export default nextConfig;
