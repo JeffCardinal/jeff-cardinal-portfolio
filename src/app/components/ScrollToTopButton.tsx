d22af92b43530c9f4ec878895511fd69bd4b9c25
Jeff Cardinal <jeffjcardinal@gmail.com>
2025-03-21 02:44:33 -0400

    scroller button

--- src/app/components/ScrollToTopButton.tsx
+++ src/app/components/ScrollToTopButton.tsx
@@ -40,20 +40,20 @@ export default function ScrollToTopButton() {
         border-[5px]`}
       style={{ zIndex: 1000 }}
     >
-        <svg
-            className="rotate-90"
-            id="a" 
-            data-name="Nav Arrow Prev" 
-            xmlns="http://www.w3.org/2000/svg" 
-            width="25" 
-            height="25" 
-            viewBox="0 0 100 100">
-            <polygon 
-              points="80 0 50 0 0 50 50 100 80 100 30 50 80 0" 
-              fill={hover ? "white" : "black"}
-              className="transition-colors duration-300 ease-in-out"
-            />
-        </svg>
+      <svg
+          className="rotate-90"
+          id="a" 
+          data-name="Nav Arrow Prev" 
+          xmlns="http://www.w3.org/2000/svg" 
+          width="25" 
+          height="25" 
+          viewBox="0 0 100 100">
+          <polygon 
+            points="80 0 50 0 0 50 50 100 80 100 30 50 80 0" 
+            fill={hover ? "white" : "black"}
+            className="transition-colors duration-300 ease-in-out"
+          />
+      </svg>
     </button>
   );
 };
