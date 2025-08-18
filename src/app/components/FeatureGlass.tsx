7b1e4f13a070fbf592edb72f43b51e6ebc2d8447
Jeff Cardinal <jeffjcardinal@gmail.com>
2025-08-17 23:57:40 -0400

    Glass features

--- src/app/components/FeatureGlass.tsx
+++ src/app/components/FeatureGlass.tsx
@@ -1,5 +1,6 @@
 'use client'
 import React, { ReactNode, useState } from 'react';
+import Image from 'next/image';
 
 export default function Feature(
   { children,
@@ -34,7 +35,7 @@ export default function Feature(
 
   return (
     <>
-      {/* <div className={`box-border p-8 flex flex-col lg:flex-row ${bgColor} ${textColor}`}> */}
+
       <div className={`relative box-border p-8 flex flex-col lg:flex-row overflow-hidden bg-sky-400 ${textColor}`}>
 
         {/* Background Video */}
@@ -51,12 +52,14 @@ export default function Feature(
 
         <div className="flex flex-1 justify-center lg:justify-end observableLeft opacity-0 z-10">
           <div className="text-black flex justify-center items-center">
-            <img
+            <Image
               className="pb-8 lg:pb-0 object-fill w-full max-w-[1000px]"
               onMouseEnter={() => setHovering(false)}
               onMouseLeave={() => setHovering(true)}
               src={hovering ? `/images/${imageName}` : `/images/${imageHoverName}`}
               alt={title}
+              width={1000}
+              height={1000}
             />
           </div>
         </div>
@@ -67,47 +70,53 @@ export default function Feature(
           <div className={`flex-col max-w-[1000px]`}>
 
             <span 
-                className="relative w-full block text-3xl lg:text-4xl font-distancia z-10 "
+                className="relative w-full block text-3xl lg:text-4xl font-distancia z-10"
                 style={{ lineHeight: 1 }}
             >
                 {title}
             </span>
             {/* Year */}
             <div className={`relative my-8`}>
-              <div className={`shadow-sm absolute -top-6 left-4 ${bgColor} ${headerTextColor} border-[1px] rounded-full px-4 py-2 text-2xl font-semibold z-10 backdrop-filter  bg-opacity-100 bg-white text-[#ea43a3] border-opacity-100`} >
+              <div className={`
+                absolute -top-6 left-4 ${bgColor} ${headerTextColor} border-[1px] rounded-full px-4 py-2 text-2xl font-semibold z-10 backdrop-filter border-white bg-opacity-100 text-[#ea43a3] border-opacity-100
+                shadow-[inset_0px_10px_12px_rgba(255,255,255,1),inset_0px_-3px_4px_rgba(200,255,255,0.1),inset_0px_-2px_1px_rgba(255,255,255,1),inset_0px_-2px_8px_rgba(222,222,222,1),0_2px_4px_rgba(200,255,255,0.15)]
+              `}>
                 Year
               </div>
-              <div className={` shadow-md p-8 border-[1px] rounded-xl backdrop-filter backdrop-blur-md ${bgColor}  bg-opacity-20 border-opacity-100`}>
+              <div className={` shadow-[inset_0px_-1px_1px_rgba(255,255,255,1),inset_0px_-2px_1px_rgba(255,255,255,1),inset_0px_-2px_8px_rgba(0,75,111,0.2),inset_0px_-4px_16px_rgba(200,255,255,1)] p-8 border-[1px] border-white rounded-xl backdrop-filter backdrop-blur-md ${bgColor}  bg-opacity-20 border-opacity-100`}>
                 <p className={`${textColor}`}>{year}</p>
               </div>
             </div>
 
             {/* Overview */}
             <div className="relative my-8">
-              <div className={`shadow-sm absolute -top-6 left-4 ${bgColor} ${headerTextColor} border-[1px] rounded-full px-4 py-2 text-2xl font-semibold z-10 backdrop-filter  bg-opacity-100 bg-white text-[#ea43a3] border-opacity-100`} >
+              <div className={`shadow-sm absolute -top-6 left-4 ${bgColor} ${headerTextColor} border-[1px] rounded-full px-4 py-2 text-2xl font-semibold z-10 backdrop-filter border-white bg-opacity-100 bg-white text-[#ea43a3] border-opacity-100
+              shadow-[inset_0px_10px_12px_rgba(255,255,255,1),inset_0px_-3px_4px_rgba(200,255,255,0.1),inset_0px_-2px_1px_rgba(255,255,255,1),inset_0px_-2px_8px_rgba(222,222,222,1),0_2px_4px_rgba(200,255,255,0.15)]`} >
                 Overview
               </div>
-              <div className={` shadow-md p-8 border-[1px] rounded-xl backdrop-filter backdrop-blur-md ${bgColor} bg-opacity-20 border-opacity-100`}>
+              <div className={` shadow-[inset_0px_-1px_1px_rgba(255,255,255,1),inset_0px_-2px_1px_rgba(255,255,255,1),inset_0px_-2px_8px_rgba(0,75,111,0.2),inset_0px_-4px_16px_rgba(200,255,255,1)] p-8 border-[1px] border-white rounded-xl backdrop-filter backdrop-blur-md ${bgColor}  bg-opacity-20 border-opacity-100`}>
                 <p className={`${textColor}`}>{description}</p>
               </div>
             </div>
 
             {/* Inspiration */}
             <div className="relative my-8">
-              <div className={`shadow-sm absolute -top-6 left-4 ${bgColor} ${headerTextColor} border-[1px] rounded-full px-4 py-2 text-2xl font-semibold z-10 backdrop-filter  bg-opacity-100 bg-white text-[#ea43a3] border-opacity-100`} >
+              <div className={`shadow-sm absolute -top-6 left-4 ${bgColor} ${headerTextColor} border-[1px] rounded-full px-4 py-2 text-2xl font-semibold z-10 backdrop-filter border-white bg-opacity-100 bg-white text-[#ea43a3] border-opacity-100
+              shadow-[inset_0px_10px_12px_rgba(255,255,255,1),inset_0px_-3px_4px_rgba(200,255,255,0.1),inset_0px_-2px_1px_rgba(255,255,255,1),inset_0px_-2px_8px_rgba(222,222,222,1),0_2px_4px_rgba(200,255,255,0.15)]`} >
                 Inspiration
               </div>
-              <div className={` shadow-md p-8 border-[1px] rounded-xl backdrop-filter backdrop-blur-md ${bgColor} bg-opacity-20 border-opacity-100`}>
+              <div className={` shadow-[inset_0px_-1px_1px_rgba(255,255,255,1),inset_0px_-2px_1px_rgba(255,255,255,1),inset_0px_-2px_8px_rgba(0,75,111,0.2),inset_0px_-4px_16px_rgba(200,255,255,1)] p-8 border-[1px] border-white rounded-xl backdrop-filter backdrop-blur-md ${bgColor}  bg-opacity-20 border-opacity-100`}>
                 <p className={`${textColor}`}>{inspiration}</p>
               </div>
             </div>
 
             {/* Tools */}
             <div className={`relative ${bottomPadding}`}>
-              <div className={`shadow-sm absolute -top-6 left-4 ${bgColor} ${headerTextColor} border-[1px] rounded-full px-4 py-2 text-2xl font-semibold z-10 backdrop-filter  bg-opacity-100 bg-white text-[#ea43a3] border-opacity-100`} >
+              <div className={`shadow-sm absolute -top-6 left-4 ${bgColor} ${headerTextColor} border-[1px] rounded-full px-4 py-2 text-2xl font-semibold z-10 backdrop-filter border-white bg-opacity-100 bg-white text-[#ea43a3] border-opacity-100
+              shadow-[inset_0px_10px_12px_rgba(255,255,255,1),inset_0px_-3px_4px_rgba(200,255,255,0.1),inset_0px_-2px_1px_rgba(255,255,255,1),inset_0px_-2px_8px_rgba(222,222,222,1),0_2px_4px_rgba(200,255,255,0.15)]`} >
                 Tools
               </div>
-              <div className={` shadow-md p-8 border-[1px] rounded-xl backdrop-filter backdrop-blur-xl ${bgColor} bg-opacity-20 border-opacity-100`}>
+              <div className={` shadow-[inset_0px_-1px_1px_rgba(255,255,255,1),inset_0px_-2px_1px_rgba(255,255,255,1),inset_0px_-2px_8px_rgba(0,75,111,0.2),inset_0px_-4px_16px_rgba(200,255,255,1)] p-8 border-[1px] border-white rounded-xl backdrop-filter backdrop-blur-md ${bgColor}  bg-opacity-20 border-opacity-100`}>
                 <p className={`${textColor}`}>{tools}</p>
               </div>
             </div>
